47ee0bb3e681ec504a4c221f2d8700a00b1c6601
Maxim <kokovin.maxim@yandex.ru>
2020-06-01 20:33:37 +0800

    The dead code was deleted

--- index.js
+++ index.js
@@ -1,21 +1,13 @@
-//const MasterSystem = require('./main.js');
 const {fork} = require('child_process');
-//const Factory = require('./realization');
 const {realizationType} = require('./config');
 const log = require('./libs/logger');
-
 const SHUTDOWN_TIMEOUT = 6000;
 const GRACEFULL_TIMEOUT = 5000;
-
 
 fork('./server.js');
 
 const communication = require(`./realization/${realizationType}`);
 communication.start('Root');
-
-
-/*Factory.create(type);
-communication.start('Root');*/
 
 process.on('SIGINT', async () => {
 	communication.exit('Root');
